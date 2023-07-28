aa41a7a0de9817328e89d04688f6fa9f0002f89f
Michael Dolgih <michael.dolgih2008@gmail.com>
2023-07-28 17:49:34 +0300

    my works

---
+++ count.py
@@ -0,0 +1,17 @@
+def summs(v,b):
+    return v+b
+ 
+
+print('monkey')
+a = input()
+b = input()
+
+otvet = summs(a,b)
+print(otvet)
+
+print('homosaphience')
+a = input()
+b = input()
+
+otvet = summs(a,b)
+print(otvet)
