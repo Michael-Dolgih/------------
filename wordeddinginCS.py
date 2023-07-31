0e35b76f126238d65ba8f73338137efebabe454e
Michael Dolgih <michael.dolgih2008@gmail.com>
2023-07-31 16:03:09 +0300

    work2

--- wordeddinginCS.py
+++ wordeddinginCS.py
@@ -1,8 +1,10 @@
+def last(last_letters):
+    if last_letters[-2:].lower() == "cs":
+        a = len(last_letters)
+        return(2**len(last_letters))
+    else:
+        return(last_letters[::-1])
+
+print('Введите любые буквы:')
 last_letters = input()
-print(last_letters[-1])
-print(last_letters[-2:])
-if last_letters[-2:].lower() == "cs":
-    a = len(last_letters)
-    print(int(a)**2)
-else:
-    print(last_letters[::-1])
+print(last(last_letters))
