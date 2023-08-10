8e7335bad31812b2be5abfa75fde71da5288fc12
Michael Dolgih <michael.dolgih2008@gmail.com>
2023-08-10 19:57:20 +0300

    workwithfilenumber2.2

--- workwithfilenumber2.py
+++ workwithfilenumber2.py
@@ -22,21 +22,27 @@ def save(res):
     file.write(res[i]+'\n')
   file.close
 
-inputs = input()
-if inputs == '-1':
-  file = open('result.txt', 'r', encoding= 'utf-8')
-  for i in file:
-    print(i)
+def massives_odinak():
+  print('massives_odinak')
+  global massive
+  massive2 = open('result.txt', 'r', encoding= 'utf-8').read().splitlines()
+  for stroka in massive:
+    if stroka not in massive2:
+      print(f'DEBUG0 : {stroka}')
+      return False
+  print(f'DEBUG1 : {massive}')
+  return True
+
+inputs = ''
 while inputs != '-1':
-    inputs = ''
-    inputs = input()
-    if inputs == '-1':
-      file = open('result.txt', 'r', encoding= 'utf-8')
-      for i in file:
-        print(i)
-    elif inputs == 'inputed':
-        pretty_print('vvedi slovo')
-        pattern = input()
-        pretty_print(inputed(pattern))
-    else:
-      pretty_print('nothing')
+  inputs = ''
+  inputs = input()
+  if inputs == 'inputed':
+    pretty_print('vvedi slovo')
+    pattern = input()
+    pretty_print(inputed(pattern))
+    if massives_odinak():
+      break
+  else:
+    pretty_print('nothing')
+pretty_print(open('result.txt', 'r', encoding= 'utf-8').read().splitlines())
